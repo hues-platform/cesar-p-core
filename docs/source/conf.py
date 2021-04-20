69b5fbad9d65522ca8f5449754d1197d0c0e4b70
LeonieFierz <56353030+LeonieFierz@users.noreply.github.com>
2021-04-20 19:11:24 +0200

    Enabling git-lfs ressources on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,14 @@ import sphinx_rtd_theme
 
 import cesarp
 
+# enable git-lfs ressources on readthedocs
+if not os.path.exists('./git-lfs'):
+    os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+    os.system('./git-lfs install')  # make lfs available in current repository
+    os.system('./git-lfs fetch')  # download content from remote
+    os.system('./git-lfs checkout')  # make local files to have the real content on them
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
